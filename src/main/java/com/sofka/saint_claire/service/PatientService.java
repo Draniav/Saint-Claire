53894cbff1b9de71a6212f6b60f58d91f70ac1b1
Alexander Reyes <daniel.alexander.reyes@gmail.com>
2022-07-30 13:18:48 -0500

    Update PatientService.java
    >delete updated
    >add appointment (has errors)

--- src/main/java/com/sofka/saint_claire/service/PatientService.java
+++ src/main/java/com/sofka/saint_claire/service/PatientService.java
@@ -1,5 +1,6 @@
 package com.sofka.saint_claire.service;
 
+import com.sofka.saint_claire.EntityDTO.Appointment;
 import com.sofka.saint_claire.EntityDTO.Patient;
 import com.sofka.saint_claire.repositoryDAO.AppointmentRepository;
 import com.sofka.saint_claire.repositoryDAO.PatientRepository;
@@ -7,6 +8,7 @@ import com.sofka.saint_claire.service.interfaces.Ipatient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.Instant;
 import java.util.List;
 
 public class PatientService implements Ipatient {
@@ -25,7 +27,7 @@ public class PatientService implements Ipatient {
     }
 
     @Override
-    @Transactional(readOnly = true)
+    @Transactional
     public Patient createPatient(Patient patient) {
         patient.setNumberOfAppointment(0);
         Patient newPatient = patientRepository.save(patient);
@@ -33,20 +35,36 @@ public class PatientService implements Ipatient {
         return newPatient;
 
     }
+    @Override
+    @Transactional
+    public Integer addNewAppointment(Integer idPatient) {
+        var PatientToVerify  = patientRepository.findById(idPatient);
+        if (PatientToVerify.isEmpty()) {
+            return null;
+        }
 
+        Patient patient = PatientToVerify.get();
+        patient.addAppointment();
+        Appointment appointment = new Appointment( null,null);
+        appointmentRepository.save(appointment);
+        return appointment.getId();
+
+    }
     @Override
     @Transactional(readOnly = true)
     public Integer getPatientByID(Integer idSpeciality, Long identification) {
         return patientRepository.validatePatientExist(idSpeciality, identification);
     }
 
-    @Override
-    public Integer addNewAppointment(Integer idPatient) {
-        return null;
-    }
+
 
     @Override
     public Patient deletePatient(Integer idPatient) {
+        var patient = patientRepository.findById(idPatient);
+        if (patient.isPresent()) {
+            patientRepository.delete(patient.get());
+            return patient.get();
+        }
         return null;
     }
 }
