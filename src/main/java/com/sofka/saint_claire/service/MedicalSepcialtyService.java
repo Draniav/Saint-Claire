c53ec632977d48775e2d176b55ded7689813b853
Alexander Reyes <daniel.alexander.reyes@gmail.com>
2022-07-30 11:42:03 -0500

    defining methods
    >working in  crud methods

--- src/main/java/com/sofka/saint_claire/service/MedicalSepcialtyService.java
+++ src/main/java/com/sofka/saint_claire/service/MedicalSepcialtyService.java
@@ -1,4 +1,48 @@
 package com.sofka.saint_claire.service;
 
-public class MedicalSepcialtyService {
+import com.sofka.saint_claire.EntityDTO.MedicalSpecialty;
+import com.sofka.saint_claire.repositoryDAO.MedicalSpecialtyRepository;
+import com.sofka.saint_claire.service.interfaces.ImedicalSpecialty;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class MedicalSepcialtyService implements ImedicalSpecialty {
+
+    @Autowired
+    private MedicalSpecialtyRepository medicalSpecialtyRepository;
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<MedicalSpecialty> getSpecialityList() {
+        return medicalSpecialtyRepository.findAll();
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public MedicalSpecialty getSpecialityByID(Integer id) {
+        var specialty = medicalSpecialtyRepository.findById(id);
+        return specialty.isPresent() ? specialty.get():null;
+    }
+
+    @Override
+    @Transactional
+    public MedicalSpecialty createSpeciality(MedicalSpecialty speciality) {
+        return null;
+    }
+
+    @Override
+    @Transactional
+    public MedicalSpecialty updateSpeciality(Integer idSpeciality, MedicalSpecialty speciality) {
+        return null;
+    }
+
+    @Override
+    public MedicalSpecialty deleteSpeciality(Integer idSpeciality) {
+        return null;
+    }
 }
--- src/main/java/com/sofka/saint_claire/service/PatientService.java
+++ src/main/java/com/sofka/saint_claire/service/PatientService.java
@@ -1,4 +1,33 @@
 package com.sofka.saint_claire.service;
 
-public class PatientService {
+import com.sofka.saint_claire.EntityDTO.Patient;
+import com.sofka.saint_claire.service.interfaces.Ipatient;
+
+import java.util.List;
+
+public class PatientService implements Ipatient {
+    @Override
+    public List<Patient> getPatientsList() {
+        return null;
+    }
+
+    @Override
+    public Patient createPatient(Patient patient) {
+        return null;
+    }
+
+    @Override
+    public Integer getPatientByID(Integer idSpeciality, Long identification) {
+        return null;
+    }
+
+    @Override
+    public Integer addNewAppointment(Integer idPatient) {
+        return null;
+    }
+
+    @Override
+    public Patient deletePatient(Integer idPatient) {
+        return null;
+    }
 }
